8b54dd1bf67016e6ec182456c98c5fda1063daa5
Olexander Podik <alp@8allocate.com>
2023-12-23 04:43:12 +0200

    remove entryComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-import { BrowserAnimationsModule }
-  from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { MaterialModule } from './material/material.module';
-import { AppComponent } from './pages/app/app.component';
-import { reducers, metaReducers } from './root-reducer';
-import { environment } from '../environments/environment';
-import { PresetEditorComponent }
-  from './pages/preset-editor/preset-editor.component';
-import { HomeComponent } from './pages/home/home.component';
-import { EffectsModule } from '@ngrx/effects';
-import { PresetEditorEffects }
-  from './pages/preset-editor/preset-editor.effects';
-import { ExerciseEditorComponent }
-  from './components/exercise-editor/exercise-editor.component';
-import { ExerciseEditorEffects }
-  from './components/exercise-editor/exercise-editor.effects';
-import { CountdownComponent } from './components/countdown/countdown.component';
-import { CountdownEffects } from './components/countdown/countdown.effects';
-import { AutofocusInputDirective } from './directives/autofocus-input.directive';
-import { StepperComponent } from './pages/stepper/stepper.component';
-import { PresetsListComponent } from './components/presets-list/presets-list.component';
-import { BottomSheetMenuComponent } from './components/bottom-sheet-menu/bottom-sheet-menu.component';
-import { RepeatsIconComponent } from './components/repeats-icon/repeats-icon.component';
-import { RepeatWithPresetIconComponent } from './components/repeat-with-preset-icon/repeat-with-preset-icon.component';
-import { PresetRepeatsIconComponent } from './components/preset-repeats-icon/preset-repeats-icon.component';
-import { SpaceTriggerDirective } from './directives/space-trigger.directive';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { TextCopyComponent } from './components/text-copy/text-copy.component';
-import { ExportComponent } from './pages/export/export.component';
-import { InfoComponent } from './pages/info/info.component';
-import { HttpClientModule } from '@angular/common/http';
-import { DeletionConfirmationComponent }
-  from './components/deletion-confirmation/deletion-confirmation.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { MaterialModule } from "./material/material.module";
+import { AppComponent } from "./pages/app/app.component";
+import { reducers, metaReducers } from "./root-reducer";
+import { environment } from "../environments/environment";
+import { PresetEditorComponent } from "./pages/preset-editor/preset-editor.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { EffectsModule } from "@ngrx/effects";
+import { PresetEditorEffects } from "./pages/preset-editor/preset-editor.effects";
+import { ExerciseEditorComponent } from "./components/exercise-editor/exercise-editor.component";
+import { ExerciseEditorEffects } from "./components/exercise-editor/exercise-editor.effects";
+import { CountdownComponent } from "./components/countdown/countdown.component";
+import { CountdownEffects } from "./components/countdown/countdown.effects";
+import { AutofocusInputDirective } from "./directives/autofocus-input.directive";
+import { StepperComponent } from "./pages/stepper/stepper.component";
+import { PresetsListComponent } from "./components/presets-list/presets-list.component";
+import { BottomSheetMenuComponent } from "./components/bottom-sheet-menu/bottom-sheet-menu.component";
+import { RepeatsIconComponent } from "./components/repeats-icon/repeats-icon.component";
+import { RepeatWithPresetIconComponent } from "./components/repeat-with-preset-icon/repeat-with-preset-icon.component";
+import { PresetRepeatsIconComponent } from "./components/preset-repeats-icon/preset-repeats-icon.component";
+import { SpaceTriggerDirective } from "./directives/space-trigger.directive";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { TextCopyComponent } from "./components/text-copy/text-copy.component";
+import { ExportComponent } from "./pages/export/export.component";
+import { InfoComponent } from "./pages/info/info.component";
+import { HttpClientModule } from "@angular/common/http";
+import { DeletionConfirmationComponent } from "./components/deletion-confirmation/deletion-confirmation.component";
 
 @NgModule({
   declarations: [
@@ -59,7 +53,7 @@ import { DeletionConfirmationComponent }
     TextCopyComponent,
     ExportComponent,
     InfoComponent,
-    DeletionConfirmationComponent
+    DeletionConfirmationComponent,
   ],
   imports: [
     BrowserModule,
@@ -75,18 +69,14 @@ import { DeletionConfirmationComponent }
     EffectsModule.forRoot([
       PresetEditorEffects,
       ExerciseEditorEffects,
-      CountdownEffects
+      CountdownEffects,
     ]),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    HttpClientModule
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [
-    ExerciseEditorComponent,
-    BottomSheetMenuComponent,
-    TextCopyComponent,
-    DeletionConfirmationComponent
-  ]
 })
-export class AppModule { }
+export class AppModule {}
